24f45a3d680c0cc37d7098fafb759a802aad8be8
akhil.potukuchi <akhil.potukuchi@gmail.com>
2017-10-29 15:12:39 +0530

    Added documentation for 'king' module

--- king.rb
+++ king.rb
@@ -1,12 +1,13 @@
 class King
-  attr_accessor  :piece_name
+  attr_accessor  :piece_name # Defines the piece name; Here, it should be 'king'
 
   def initialize
-    @piece_name = 'king'
+    @piece_name = 'king' # Assigns 'king' to piece_name
   end
 
-  def validate current_position, destination
-      return true if (current_position[0] - destination[0]).abs <= 1 && (current_position[1] - destination[1]).abs <=1
+  def validate current_position, destination # Validation function for next move 
+      return true if (current_position[0] - destination[0]).abs <= 1 && (current_position[1] - destination[1]).abs <=1 # King can only move a distance of one on the board
       false
     end
 end
+# :D
